d96a70ae6189e944479ba772ad0a6fa836e7e55c
Robin <robinp31@uw.eduL>
2023-11-12 21:26:59 -0700

    completed HW

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,19 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+document.getElementById("more-info").addEventListener('click', (event) => {
+  event.preventDefault()
+  alert("Here's some info");
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+document.getElementById('dog').addEventListener('click', (event) => {
+ 
+ 
+  alert('Bow wow!');
+  event.stopPropagation()
+  // document.getElementById('cat').addEventListener('click', ()=>{
+  //   alert("meow!")
+  // })
+});
